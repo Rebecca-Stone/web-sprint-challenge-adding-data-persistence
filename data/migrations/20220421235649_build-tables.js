ec143ad92d0d285ede8775fb9ecd541b7939e36b
Rebecca-Stone <primstone16@gmail.com>
2022-04-21 19:49:30 -0500

    worked on migration and put some more directions in the routers

--- data/migrations/20220421235649_build-tables.js
+++ data/migrations/20220421235649_build-tables.js
@@ -3,29 +3,46 @@ exports.up = function (knex) {
     knex.schema
       //   table 1
       // - [ ] A **project** is what needs to be done and is stored in a `projects` table with the following columns:
-      .createTable(() => {
-        //   - [ ] `project_id` - primary key
-        //   - [ ] `project_name` - required
-        //   - [ ] `project_description` - optional
-        //   - [ ] `project_completed` - the database defaults it to `false` (integer 0) if not provided
+      .createTable("project", (tbl) => {
+        //   - [ ] `` - primary key
+        tbl.increments("project_id");
+        //   - [ ] `` - required
+        tbl.string("project_name").notNullable();
+        //   - [ ] `` - optional
+        tbl.string("project_description");
+        //   - [ ] `` - the database defaults it to `false` (integer 0) if not provided
+        tbl.boolean("project_completed");
       })
 
       //   table 2
       // - [ ] A **resource** is anything needed to complete a project and is stored in a `resources` table with the following columns:
-      .createTable(() => {
-        //   - [ ] `resource_id` - primary key
-        //   - [ ] `resource_name` - required and unique
-        //   - [ ] `resource_description` - optional
+      .createTable("resource", (tbl) => {
+        //   - [ ] `` - primary key
+        tbl.increments("resource_id");
+        //   - [ ] `` - required and unique
+        tbl.string("resource_name").notNullable().unique();
+        //   - [ ] `` - optional
+        tbl.string("resource_description");
       })
 
       //   table 3
       // - [ ] A **task** is one of the steps needed to complete a project and is stored in a `tasks` table with the following columns:
-      .createTable(() => {
-        //   - [ ] `task_id` - primary key
-        //   - [ ] `task_description` - required
-        //   - [ ] `task_notes` - optional
-        //   - [ ] `task_completed` - the database defaults it to `false` (integer 0) if not provided
-        //   - [ ] `project_id` - required and points to an actual `project_id` in the `projects` table
+      .createTable("task", (tbl) => {
+        //   - [ ] `` - primary key
+        tbl.increments("task_id");
+        //   - [ ] `` - required
+        tbl.string("task_description").notNullable();
+        //   - [ ] `` - optional
+        tbl.string("task_notes");
+        //   - [ ] `` - the database defaults it to`false`(integer 0) if not provided
+        tbl.boolean("task_completed");
+        //   - [ ] `` - required and points to an actual `project_id` in the `projects` table
+        tbl
+          .integer("project_id")
+          .unsigned()
+          .notNullable()
+          .references("project_id")
+          .inTable("projects");
       })
 
     // - [ ] A **resource assignment** connects a resource and a project, and is stored in a `project_resources` table. You decide what columns to use.
@@ -36,10 +53,10 @@ exports.down = function (knex) {
   return (
     knex.schema
       //   **project**
-      .dropTableIfExists()
+      .dropTableIfExists("project")
       //   **resource**
-      .dropTableIfExists()
+      .dropTableIfExists("resource")
       //   **task**
-      .dropTableIfExists()
+      .dropTableIfExists("task")
   );
 };
